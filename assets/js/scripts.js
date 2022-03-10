ccdb13d04ccdc94ae7ed298c82ddc99a290371c3
Shamsul Islam Sujon <sujon2762@gmail.com>
2022-03-11 03:03:04 +0600

    Update: Search form toggle

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,68 +2,72 @@
 
 // Header show/hide on scroll
 (function () {
-    var headroom = new Headroom(document.querySelector(".navbar"), {
-        tolerance: 5,
-        offset: 205,
-    });
-    headroom.init();
+	var headroom = new Headroom(document.querySelector(".navbar"), {
+		tolerance: 5,
+		offset: 205,
+	});
+	headroom.init();
 })();
 
 // Animate on Scroll
 AOS.init({
-    once: true,
-    easing: "ease-in-out-quad",
-    delay: 100,
-    offset: 50,
+	once: true,
+	easing: "ease-in-out-quad",
+	delay: 100,
+	offset: 50,
 });
 
-// Search
+// Search form toggle
 const searchBtn = document.querySelector(".search-btn");
 const searchInput = document.querySelector(".search-input");
 
 searchBtn.addEventListener("click", function () {
-    searchInput.classList.toggle("open");
+	searchInput.classList.toggle("open");
 
-    setTimeout(function () {
-        searchInput.focus();
-    }, 50);
+	setTimeout(function () {
+		searchInput.focus();
+	}, 50);
 });
 
-document.addEventListener("mouseup", function (e) {
-    if (e.target !== searchInput) {
-        searchInput.classList.remove("open");
-    }
+// Hide search form on click outside
+document.addEventListener("click", (e) => {
+	let el = e.target.closest(".search-btn");
+	let el2 = e.target.closest(".search-input");
+	if (el2 == null && el == null) {
+		searchInput.classList.remove("open");
+	}
 });
 
+// Hide search form on press ESC key
 document.onkeyup = function (e) {
-    if (e.keyCode == 27) {
-        searchInput.classList.remove("open");
-    }
+	if (e.keyCode == 27) {
+		searchInput.classList.remove("open");
+	}
 };
 
 // Blog carousel
 const blogCarousel = new Swiper(".articles-carousel", {
-    slidesPerView: 3,
-    spaceBetween: 40,
-    speed: 600,
-    watchOverflow: true,
-    centeredSlides: false,
-    simulateTouch: false,
-    breakpoints: {
-        320: {
-            slidesPerView: 1.1,
-            spaceBetween: 20,
-            centeredSlides: true,
-        },
-        768: {
-            slidesPerView: 2.05,
-            spaceBetween: 30,
-            centeredSlides: false,
-        },
-        1024: {
-            slidesPerView: 3,
-            spaceBetween: 40,
-            centeredSlides: false,
-        },
-    },
+	slidesPerView: 3,
+	spaceBetween: 40,
+	speed: 600,
+	watchOverflow: true,
+	centeredSlides: false,
+	simulateTouch: false,
+	breakpoints: {
+		320: {
+			slidesPerView: 1.1,
+			spaceBetween: 20,
+			centeredSlides: true,
+		},
+		768: {
+			slidesPerView: 2.05,
+			spaceBetween: 30,
+			centeredSlides: false,
+		},
+		1024: {
+			slidesPerView: 3,
+			spaceBetween: 40,
+			centeredSlides: false,
+		},
+	},
 });
