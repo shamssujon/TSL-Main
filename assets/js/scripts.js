51e648e91e3b972a8112907b17c1b2dac5ed7745
shamssujon <sujon2762@gmail.com>
2022-06-01 13:35:04 +0600

    About page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -71,3 +71,32 @@ const blogCarousel = new Swiper(".articles-carousel", {
 		},
 	},
 });
+
+// Enabling Bootstrap Tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+
+const counterUp = window.counterUp.default
+
+const callback = entries => {
+	entries.forEach(entry => {
+		const el = entry.target
+		if (entry.isIntersecting && !el.classList.contains('is-visible')) {
+			counterUp(el, {
+				duration: 1000,
+				delay: 45,
+			})
+			el.classList.add('is-visible')
+		}
+	})
+}
+
+const IO = new IntersectionObserver(callback, { threshold: 1 })
+
+const el = document.querySelectorAll('.counter')
+
+el.forEach((el) => {
+	IO.observe(el)
+})
